2207a14df4d06fcf8d8a6676bb426c685af56b3b
Julius Olamide <juliusolajumokecoder@gmail.com>
2022-04-27 14:59:29 +0100

    new changes occured

--- src/App.tsx
+++ src/App.tsx
@@ -1,90 +1,79 @@
 import React from 'react';
+import { Route, Routes, useParams } from "react-router-dom";
 import { useState, useEffect } from 'react';
 import './App.css';
 import { FaCalendarAlt } from "react-icons/fa";
 import axios from 'axios';
 
-function App() {
+export default function App() {
+  return (
+    <div className="App">      
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/events/:email/:token" element={<Events />} />
+      </Routes>
+    </div>
 
-  const [ events, setEvent ] = useState<any[]>([]);
+  );
+}
 
-  // useEffect(() => {
-  //   axios
-  //   .get('https://julius-calendar-backend-api.herokuapp.com/google')
-  //   .then((response) => {
-  //     if(response && response.data){
-  //       console.log("response", response.data.items);
-  //       setEvent(response.data.items); 
-  //     } else {
-  //       console.log("no response");
-  //     }        
-      
-  //   })
-  //   .catch((ex) => {
-  //     let error = axios.isCancel(ex)
-  //       ? 'Request Cancelled'
-  //       : ex.code === 'ECONNABORTED'
-  //       ? 'A timeout has occurred'
-  //       : ex.response.status === 404
-  //       ? 'Resource Not Found'
-  //       : 'An unexpected error has occurred';
-  //     console.log("This is an error....");
-  //   });
-    
-  // }, []);
+const Home = () => {
 
   function callGoogle() {
     window.location.href = 'https://julius-calendar-backend-api.herokuapp.com/google';
     //window.open('http://localhost:5000/google', '_blank');
   }
 
-  function getEvents(){
-    const token = localStorage.getItem("accessToken");
-    const email = localStorage.getItem("email");
-    if(token && email){
-      axios
-      .get(`https://www.googleapis.com/calendar/v3/calendars/${email}/events`, {
-        headers: {
-          'Authorization': `Bearer ${token}`,
-          'Accept': 'application/json',
-        }
-      })
-      .then((response) => {
-        if(response){
-          console.log("response", response.data);
-          setEvent(response.data.items); 
-        } else {
-          console.log("no response");
-        }              
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-    } else {
-      console.log("No token and email");
-    }
-  }
-  
-  useEffect(() => {
-    getEvents();
-  }, []); 
-  
   return (
-    <div className="App">
+    <div className="">
       <h1>Calendar Application <FaCalendarAlt /></h1>
       <button onClick={ () => callGoogle() } className="btnView">View Events</button>
-      <div>
-        {events && events.length > 0 && 
-          <>
-            {events.map((event, index) => (
-
-              <h1 key={index}>{event.summary}</h1>
-            )) }
-          </>
-        }
-      </div>
     </div>
   );
-}
+};
 
-export default App;
+const Events = () => {
+
+  const params = useParams();
+  console.log("these are the params", params);
+
+  const [ events, setEvent ] = useState<any[]>([]);
+
+  function getEvents(){
+    const token = params.token;
+    axios
+    .get(`https://www.googleapis.com/calendar/v3/calendars/${params.email}/events`, {
+      headers: {
+        'Content-Length': '0',
+        'Authorization': `Bearer ${token}`
+      },
+      timeout: 50000,
+    })
+    .then((response) => {
+      setEvent(response.data.items);
+    })
+    .catch((ex) => {
+      console.log(ex);
+    });
+  } 
+
+  useEffect(() => {
+    getEvents();
+  }); 
+  return (
+    <div>
+      <h1>Calendar Events</h1>
+      {events && events.length > 0 && 
+        <>
+          {events.map((event, index) => (
+            <div className='row'>
+                <h1 key={index}>{event.summary}</h1>
+                <p key={index}>{event.summary}</p>
+            </div>
+          )) }
+        </>
+      }
+    </div>
+  );
+};
+
